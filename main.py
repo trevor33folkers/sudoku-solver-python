b7bb841b64959047a60d78d408e36eec0faaa3d0
Trevor Folkers <travesty3313@gmail.com>
2024-02-19 17:58:36 -0500

    grid drawn

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ green = (0,255,0)
 red = (255,0,0)
 
 #screen object
-screen = pygame.display.set_mode((800,800))
+screen = pygame.display.set_mode((1000,800))
 
 #fill screen
 screen.fill(white)
@@ -17,8 +17,23 @@ pygame.display.flip()
 
 running = True
 
+#draw grid function
+def drawGrid():
+    for i in range(0, 10):
+        if (i%3 == 0):
+            pygame.draw.line(screen, black, (40+80*i, 40), (40+80*i, 760), 4)
+            pygame.draw.line(screen, black, (40, 40 + 80*i), (760, 40 + 80*i), 4)
+        else: 
+            pygame.draw.line(screen, black, (40+80*i, 40), (40+80*i, 760), 2)
+            pygame.draw.line(screen, black, (40, 40 + 80*i), (760, 40 + 80*i), 2)
+    pygame.display.update()
+
+        
+
 #main loop
 while running:
+    drawGrid()
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+    pygame.display.update()
